6c16a05ae47c32f9ef0a407500fa58f2db2a4198
Anthony Mineo <anthonygusmineo@gmail.com>
2025-02-12 21:51:39 -0500

    Generic MonoBehaviour Singleton

---
+++ subvrsivetestunity/Assets/_project/Scripts/MonoBehaviourSingleton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class MonoBehaviourSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+
+                if (_instance == null)
+                {
+                    GameObject singletonObject = new GameObject(typeof(T).Name);
+                    _instance = singletonObject.AddComponent<T>();
+
+                    DontDestroyOnLoad(singletonObject);
+                }
+            }
+            return _instance;
+        }
+    }
+
+    private static T _instance;
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
