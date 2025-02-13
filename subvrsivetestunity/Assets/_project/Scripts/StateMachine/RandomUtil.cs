f3d7d20362f606feb3322f64b98a7c4cfebfdf08
Anthony Mineo <anthonygusmineo@gmail.com>
2025-02-12 21:53:13 -0500

    Random Utility Script

---
+++ subvrsivetestunity/Assets/_project/Scripts/StateMachine/RandomUtil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class RandomUtil
+{
+    private static readonly Lazy<RandomUtil> lazyInstance =
+        new Lazy<RandomUtil>(() => new RandomUtil());
+
+    public static RandomUtil Instance => lazyInstance.Value;
+
+    private readonly Random random;
+
+    private RandomUtil()
+    {
+        random = new Random();
+    }
+
+    private RandomUtil(int seed)
+    {
+        random = new Random(seed);
+    }
+
+    public int Next()
+    {
+        return random.Next();
+    }
+
+    public int Next(int maxValue)
+    {
+        return random.Next(maxValue);
+    }
+
+    public int Next(int minValue, int maxValue)
+    {
+        return random.Next(minValue, maxValue);
+    }
+}
